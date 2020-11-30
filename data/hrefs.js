73ad58276e9b869bd90fccee498ff44e8f0c57d0
Alec Leong <leongalec@gmail.com>
2020-11-30 13:29:21 -0800

    <Index /> data file.

---
+++ data/hrefs.js
@@ -0,0 +1,65 @@
+export default [
+  {
+    name: '/animations',
+    files: [
+      'email-v1.json',
+      'email-v2.json',
+      'github.json',
+      'http404.json',
+      'linkedin.json',
+      'pause-and-play.json',
+      'pinterest.json',
+      'twitch-v1.json',
+      'twitch-v2.json',
+    ],
+  },
+  {
+    name: '/education',
+    files: [
+      'golde-gate-bridge.jpg',
+      'sjsu-logo.png',
+      'hack-reactor-logo.svg',
+    ],
+  },
+  {
+    name: '/images',
+    files: [
+      'logo.ico',
+      'logo.jpg',
+      'profile-picture.png',
+    ],
+  },
+  {
+    name: '/particles',
+    files: [
+      'nucleus.png',
+      'snow-flake.png',
+    ],
+  },
+  {
+    name: '/projects',
+    files: [
+      'covid19-twilio-sms-lighthouse.html',
+      'bitcoin-price-index-video.mp4',
+      'travelsite-reviews-video.mp4',
+      'bitcoin-price-index-image.png',
+      'covid19-twilio-sms-image.png',
+      'travelsite-reviews-image.png',
+    ],
+  },
+  {
+    name: '/resume',
+    files: [
+      'Leong-Alec-resume.pdf',
+    ],
+  },
+  {
+    name: '/work',
+    files: [
+      'amc-logo.png',
+      'ellie-and-eva-logo.png',
+      'klarity-logo.png',
+      'razu-logo.svg',
+    ],
+  },
+];
